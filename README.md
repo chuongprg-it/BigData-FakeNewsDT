d53f15c7457d34b982b847d54c489a829dc92668
chuongit-prg <20522092@gm.uit.edu.vn>
2024-01-28 01:47:24 +0700

    merge code

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# BigData-FakeNewDT
+# Fake News Detection
+
+This project aims to detect fake news using Big Data technologies. We use various machine learning models and natural language processing techniques to analyze and classify news articles.
+
+## Table of Contents
+
+- [Fake News Detection](#fake-news-detection)
+  - [Table of Contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Technologies Used](#technologies-used)
+  - [Data Collection](#data-collection)
+  - [Data Processing](#data-processing)
+  - [Model Training](#model-training)
+  - [Results](#results)
+  - [Conclusion](#conclusion)
+- [Running the Code](#running-the-code)
+
+## Introduction
+
+Fake news is a significant issue in today's digital age. This project aims to tackle this problem using Big Data technologies and machine learning.
+
+## Technologies Used
+
+We use a combination of Python, Apache Kafka, and PySpark for data processing and model training.
+
+## Data Collection
+
+We collected our data from various online sources. The data consists of news articles labeled as 'true' or 'fake'. You can find the datasets in the [datasets](BigData-FakeNewDT/datasets) directory.
+
+## Data Processing
+
+We use Apache Kafka for real-time data streaming and PySpark for data processing. You can find the code for this in the [kafka](BigData-FakeNewDT/kafka) directory.
+
+## Model Training
+
+We trained our model using PyTorch. The trained model can be found in the [model](BigData-FakeNewDT/model) directory.
+
+## Results
+
+Our model achieved an accuracy of 91.78% on the test dataset.
+
+## Conclusion
+
+This project demonstrates the potential of using Big Data technologies in tackling the issue of fake news. Future work could involve improving the model's accuracy and using more diverse data sources.
+
+# Running the Code
+
+Follow these steps to run the code:
+
+1. **Navigate to the project directory:**
+
+```sh
+cd BigData-FakeNewDT
+```
+
+2. **Activate the virtual environment:**
+```sh
+source venv/bin/activate
+```
+3. **Install the required dependencies:**
+```sh
+pip install -r requirements.txt
+```
+4. **Run the application:**
+```sh
+python application.py
+```
--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 # import numpy as np
 # import pandas as pd
 # import matplotlib.pyplot as plt
@@ -12,6 +13,7 @@ from torchtext import data
 # from torcheval.metrics import binary_accuracy
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 #
 device = torch.device("cpu")
@@ -63,7 +65,7 @@ class LSTMNet(nn.Module):
     
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("keepitreal/vietnamese-sbert")
+tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
 vocab_size = tokenizer.vocab_size
 embedding_dim = 300
 hidden_dim = 64
@@ -73,7 +75,7 @@ bidirectional = True
 dropout = 0.2
 
 model = LSTMNet(vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, bidirectional, dropout)
-model.load_state_dict(torch.load('I:\CODE\PROJECT_COURSE\BIGDATA\FakeNewDT_Vietnamese\model\model_0.9178659543395042.pth', map_location=device))
+model.load_state_dict(torch.load('./model/model_0.9178659543395042.pth', map_location=device))
 model = model.to(device)
 model.eval()
 
@@ -97,6 +99,88 @@ def input_test(text, tokenizer, model, device):
 
     return binary_predictions[0][0]
 
+
+
+import findspark
+findspark.init()
+import pyspark
+from pyspark.sql import SparkSession 
+
+import numpy as np
+import transformers
+import pandas as pd
+# import matplotlib.pyplot as plt
+import torch
+# import seaborn as sns
+from pyspark.sql.types import ArrayType, IntegerType
+
+from torch.utils.data import Dataset
+import torch.nn.utils.rnn as rnn_utils
+# import matplotlib.pyplot as plt
+import time
+from pyspark.sql.functions import *
+# import seaborn as sns
+import statsmodels.api as sm
+import pyspark
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession, SQLContext
+
+from pyspark.ml.feature import StringIndexer, VectorIndexer, StringIndexerModel, IndexToString
+from pyspark.sql import SparkSession
+
+scala_version = '2.12'  # your scala version
+spark_version = '3.5.0' # your spark version
+packages = [
+    f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
+    'org.apache.kafka:kafka-clients:3.6.0' #your kafka version
+]
+spark = SparkSession.builder.master("local").appName("kafka-example").config("spark.jars.packages", ",".join(packages)).getOrCreate()
+
+sqlContext = spark.sparkContext
+
+from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, FloatType
+
+# Define the schema for your DataFrame
+schema = StructType([
+    StructField("post_message", StringType(), True),
+])
+
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
+# Specify the Kafka parameters
+kafka_params = {
+    "kafka.bootstrap.servers": "localhost:9092",  # Change this to your Kafka broker
+    "subscribe": "FakeNewDT",               # Change this to your Kafka topic
+    "startingOffsets": "earliest"
+}
+
+stream_model = LSTMNet(vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, bidirectional, dropout)
+device = torch.device("cpu")
+stream_model = stream_model.to(device)
+stream_model.load_state_dict(torch.load('./model/model_0.9178659543395042.pth',map_location=torch.device('cpu')))
+stream_model.eval()
+
+def stream_predict(text, tokenizer, model, device):
+    tokens = tokenizer.tokenize(text)
+    ids = tokenizer.convert_tokens_to_ids(tokens)
+    ids = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
+
+    ids = ids.to(device)
+
+    # Get the length of the input sequence
+    length = torch.tensor(ids.shape[1], dtype=torch.long).unsqueeze(0)
+
+    # Evaluate the model on the input text
+    with torch.no_grad():
+        model.eval()
+        predictions = model(ids, length)
+
+
+    binary_predictions = torch.round(predictions).cpu().numpy()
+
+    return binary_predictions[0][0]
+
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -111,15 +195,49 @@ def check():
             result = "Tin giả"
         else:
             result = "Tin thật"  # Thay đổi kết quả tùy thuộc vào quá trình kiểm tra
-        return render_template('index.html', results=[{'input_text': input_text, 'detection_value': result}])
+        return render_template('index.html', result=result, input_text=input_text)
     
 @app.route('/streaming')
 def streaming():
-    random_data = [
-        {'STT': 1, 'Sentence': 'This is a random sentence', 'Value Detection': 'True'},
-        # Các dòng dữ liệu khác nếu có
-    ]
-    return render_template('streaming.html', random_data=random_data)
-    
+    kafka_stream_df = (
+    spark.read.format("kafka")
+    .option("kafka.bootstrap.servers", kafka_params["kafka.bootstrap.servers"])
+    .option("subscribe", kafka_params["subscribe"])
+    .option("startingOffsets", kafka_params["startingOffsets"])
+    .load()
+    .selectExpr("CAST(value AS STRING)")
+    .select(from_json("value", schema).alias("data"))
+    .select("data.*")
+    )
+# Apply the UDF to create a new column 'predictions'
+    data = kafka_stream_df.toPandas()['post_message']
+    data = data.head(100)
+    import pandas as pd
+    from time import sleep
+    from IPython.display import display, clear_output
+
+    # Tạo DataFrame trống
+    df = pd.DataFrame(columns=['post_message', 'predictions'])
+    sent = []
+    pred = []
+    for i in range(len(data)):
+        result = stream_predict(data[i], tokenizer, stream_model, device)
+        # df = df.append({'post_message': data[i], 'predictions': result}, ignore_index=True)
+        sent.append(data[i])
+        pred.append(result)
+        render_template(
+            'streaming.html',
+            sent=sent,
+            pred=pred
+        )
+        # print(sent)
+        print(pred)
+        # sleep(3)
+        # socketio.emit('update', {'sent': sent, 'pred': pred})
+        # socketio.sleep(1)
+        clear_output(wait=True)
+    return render_template('streaming.html', sent=sent, pred=pred)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
